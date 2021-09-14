cbb8a17b28886a99dadfa6b9d201782dabcb6971
ProTextD <188656397@qq.com>
2021-09-14 08:49:22 +0800

    test

--- LearnVuejs/learnaxios/src/networks/request.js
+++ LearnVuejs/learnaxios/src/networks/request.js
@@ -7,6 +7,30 @@ export function request(config){
         timeout : 5000
     })
 
+    //2、axios的拦截器
+    // axios.interceptors //全局的拦截器
+    // instance.interceptors //实例拦截器
+    //2.1. 请求拦截的作用
+    instance.interceptors.request.use(config =>{ //对请求的拦截
+        console.log(config)
+        //1、比如config中的一些信息不符合服务器的要求
+
+        //2、比如每次发送网络请求时，都希望在界面中显示一个请求的图标
+
+        //3、某些网络请求(比如登录(token)),必须携带一些特殊的信息
+        return config //一定要把config返回出去，否则被中断拦截，不能正常响应
+    } , err =>{
+        console.log(err)
+    })
+
+    //2.2. 响应拦截
+    instance.interceptors.response.use(res =>{
+        console.log(res)
+        return res.data
+    } , err =>{
+        console.log(err)
+    })
+
     //发送真正的网络请求
     return instance(config) //返回的是一个Promise对象
 }
